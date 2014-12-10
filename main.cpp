00f34bd36dab3f43fc0a90643c74f2dcbc9ec8e6
Ananya Bahadur <ananya95@gmail.com>
2014-12-10 21:59:49 +0530

    Added explanatory comments. I rest my case.
    You guys finish. Next up is sorting answers.len - take least and set as next node

--- main.cpp
+++ main.cpp
@@ -25,60 +25,54 @@ public:
   }
 };
 
-<<<<<<< HEAD
-int main()
+struct history
 {
-  int N; 
-  std::cin>>N;//no. of vertices
-
-=======
-struct history{
   int len, prev;
 }
 
 int main(){
   int N; std::cin>>N;
->>>>>>> visitedList
   graph data(N);
-
   for (int i=0; i<N*N; i++)//read the adj. matrix
     std::cin>>data[i];
-
   //std::cout<<data(1, 1)<<std::endl<<data(2, 2);
-<<<<<<< HEAD
-  int sn, en;
-  char visited[N];
-  std::cin>>sn>>en;//read start and end node
-  //implement visitedSet as BITarray <char> type
-  //how to store path info?
-=======
   int sn, en, visited_size = 0;//as soon as visited size==N, break
+  //answer is a pair of {len, previous}
   history answer[N];
+  //visited is a BitArray. If vertex 4,2 are visited then
+  //visited == {0,0,1,0,1,...}
+  //visited means fully checked (compare with wikipedia algo)
   int visited[N];
-  for (int i=0; i<N; i++){
-    answer[i].len = -1;
-    visited[i] = 0;
-  }
+  //initialising
+  for (int i=0; i<N; i++)
+    {
+      answer[i].len = -1; //equivalent to infinity
+      visited[i] = 0;
+    }
   cin>>sn>>en;
   //start the algo!
   answer[i] = 0;
-  int curn=sn, new_len;//current_node
-  while(visited_size <= N){
-    for (int i=0; i<N; i++){
-      if (!visited[i] && data(curn, i) >= 0){
-	new_len = answer[curn].len + data(curn, i);
-	if (new_len < answer[i].len || answer[i] == -1){
-	  answer[i].len = new_len;
-	  answer[i].prev = curn;
+  int curn=sn, new_len;
+  while(visited_size <= N)
+    {
+      for (int i=0; i<N; i++)
+	{
+	  if (!visited[i] && data(curn, i) >= 0)
+	    //!visited and edge exists
+	    {
+	      new_len = answer[curn].len + data(curn, i);
+	      if (new_len < answer[i].len || answer[i] == -1)
+		//new_len < old_len OR old_len == infinity
+		{
+		  answer[i].len = new_len;
+		  answer[i].prev = curn;
+		}
+	      //else do nothing
+	    }
 	}
-      }
+      visited_size++;
+      visited[curn] = 1;
+      //select new node
     }
-    visited_size++;
-    visited[curn] = 1;
-    //select new node
-  }
-    
->>>>>>> visitedList
   return 0;
 }
-  
