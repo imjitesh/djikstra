041a32eb27e5d1640162fca2adfcf33a87a4ec2a
Ananya Bahadur <ananya95@gmail.com>
2014-12-10 15:11:34 +0530

    Added ignore list Just run make to compile the project Addded main.cpp        class graph <vector of N*N len>        reads stdin  Changes to be committed: 	new file:   .gitignore 	new file:   Makefile 	new file:   main.cpp

---
+++ Makefile
@@ -0,0 +1,6 @@
+all:
+	g++ -g -Wall -o main.o main.cpp
+run:
+	./main
+clean:
+	rm main
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+//not using namespace std!!
+class graph{
+  std::vector<int> gmat;
+  const int N;
+public:
+  graph(int n): N(n){
+    gmat = std::vector<int>(N*N);
+  }
+  int& operator()(unsigned int r, unsigned int c){
+    return gmat[(r-1)*N+c-1];
+  }
+  int& operator[](unsigned int key){
+    return gmat[key];
+  }
+};
+
+int main(){
+  int N; std::cin>>N;
+  graph data(N);
+  for (int i=0; i<N*N; i++)
+    std::cin>>data[i];
+  //std::cout<<data(1, 1)<<std::endl<<data(2, 2);
+  int sn, en;
+  char visited[N];
+  return 0;
+}
+  
